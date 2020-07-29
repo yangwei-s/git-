b0813346fd5f0ecab775495303f45bfff2a2c4f3
yangwei <1293343451@qq.com>
2020-07-29 20:28:42 +0800

    完成index页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -25,4 +25,22 @@ $.ajaxPrefilter(function(options) {
   //     location.href = '/login.html'
   //   }
   // }
+
+  //  统一为有权限的接口没设置请求头
+   if(options.url.indexOf('/my/')!==-1){
+     options.headers={
+      Authorization: localStorage.getItem('token') || ''
+     }
+
+   }
+
+  //  全局统一挂载complete回调函数
+  options.complete=function(res){
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+          // 1. 强制清空 token
+          localStorage.removeItem('token')
+          // 2. 强制跳转到登录页面
+          location.href = '/login.html'
+        }
+  }
 })
